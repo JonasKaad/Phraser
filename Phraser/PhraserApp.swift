7f0a1a256e37e6d38063daa4fb82cca3a5c9c28a
Jonas Kaad <jonaskaad@gmail.com>
2024-11-06 23:31:57 +0900

    Created category model

--- Phraser/PhraserApp.swift
+++ Phraser/PhraserApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct PhraserApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Category.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
