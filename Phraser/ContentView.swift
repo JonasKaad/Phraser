d4bddab0478ca0a3c7f39e81e3e3b1f38d9ec2f9
Jonas Kaad <jonaskaad@gmail.com>
2024-11-06 23:33:17 +0900

    Updated UI with category view

--- Phraser/ContentView.swift
+++ Phraser/ContentView.swift
@@ -10,52 +10,112 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @Environment(\.colorScheme) var colorScheme
+    @Query private var categories: [Category]
+    let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+    ]
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        
+//        NavigationSplitView {
+//            List(categories) { category in
+//                VStack {
+//
+//                    HStack
+//                    {
+//                        CategoryView(category: category)
+//                        CategoryView(category: category)
+//                    }
+//                }
+//                Spacer()
+//            }
+//        }detail: {
+//            Text("Create a category")
+//        }
+//        categoryCard
+        VStack(alignment: .leading, spacing: 20) {
+                    Text("Phrasebook")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(.leading)
+                        .safeAreaPadding(.top, 20)
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(categories) { category in
+                    CategoryView(category: category)
                 }
-                .onDelete(perform: deleteItems)
+                // Add button
+                Button(action: createCategory) {
+                    AddCategoryView()
+                }
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+            
+        }
+        .padding()
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
+    
+    // private view that contains the category name and the emoji/logo
+    struct CategoryView: View {
+        var category: Category
+        var body: some View {
+            VStack(spacing: 10) {
+                Image(systemName: category.logo)
+                    .font(.system(size: 50))
+                    .foregroundColor(.blue)
+                .padding(8)
+                Text(category.name)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+            }
+            .frame(maxWidth: 180, minHeight: 130)
+            .background(Color(UIColor.systemBackground))
+            .cornerRadius(10)
+            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+    
+    struct AddCategoryView: View {
+        var body: some View {
+            VStack(spacing: 10) {
+                Image(systemName:"plus")
+                    .font(.system(size: 50))
+                    .foregroundStyle(.blue)
+                
+                .padding(8)
+                Text("Add")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.blue)
             }
+            .frame(maxWidth: 180, minHeight: 130)
+            .background(Color.blue.opacity(0.12))
+            .background(Color(UIColor.systemBackground))
+            .cornerRadius(10)
+            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+        }
+            
+    }
+    
+    
+
+    private func createCategory() {
+        withAnimation(.spring()) {
+        
+            // prompt the user to enter name for the category and select an emoji
+            let categoryName = "Test"
+            let categoryLogo = "folder"
+            let newCategory = Category(timestamp: Date(), id: UUID(), name: categoryName, logo: categoryLogo)
+            
+            modelContext.insert(newCategory)
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(for: [Category.self, Phrase.self], inMemory: true)
 }
