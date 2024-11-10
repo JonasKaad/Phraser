209aeef6f7665614239d151ac40a07a494265224
Jonas Kaad <jonaskaad@gmail.com>
2024-11-10 18:41:19 +0900

    Updated view for displaying phrases

---
+++ Phraser/View/PhraseView.swift
@@ -0,0 +1,78 @@
+//
+//  PhraseView.swift
+//  Phraser
+//
+//  Created by Jonas Kaad on 07/11/2024.
+//
+
+import SwiftUI
+import SwiftData
+import AVFoundation
+
+struct PhraseView: View {
+    @Environment(\.modelContext) private var modelContext
+    @ObservedObject var category: Category
+    @Query var phrases: [Phrase]
+
+    let synthesizer = AVSpeechSynthesizer()
+    @State private var isShowingAddSheet = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack(spacing: 10) {
+                    Image(systemName: category.logo)
+                        .font(.system(size: 30))
+                        .foregroundColor(.blue)
+                        .padding(8)
+                    Text(category.name)
+                        .font(.callout)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                }
+                VStack(alignment: .leading, spacing: 20) {
+                    ScrollView {
+                        if let phrases = category.phrases, !phrases.isEmpty {
+                            ForEach(phrases) { phrase in
+                                DisplayPhraseView(category: category, phrase: phrase, synthesizer: synthesizer)
+                            }
+                        } else {
+                            Text("No phrases for \(category.name)")
+                            Text("Add some phrases!")
+                        }
+                            
+                            Button(action: {
+                                isShowingAddSheet.toggle()}) {
+                                    CategoryAddView()
+                                }
+                                .padding()
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $isShowingAddSheet) {
+            CreateNewPhraseView(isPresented: $isShowingAddSheet.wrappedValue, createPhrase: createPhrase)
+        }
+    }
+    
+    private func createPhrase(_text: String, _phoentic: String, _translation: String) {
+        let phraseText = _text
+        let phraseTranslation = _translation
+        let phrasePhoentic = _phoentic
+        let newPhrase = Phrase(text:phraseText , id: UUID(), category:category , timestamp:Date() , phoentic:phrasePhoentic , translation: phraseTranslation)
+        
+        modelContext.insert(newPhrase)
+        
+        // Add the new phrase to the category's phrases array
+        if category.phrases == nil {
+            category.phrases = [newPhrase]
+        } else {
+            category.phrases?.append(newPhrase)
+        }
+    }
+}
+
+#Preview {
+    PhraseView(category: Category(timestamp: Date(), id: UUID(), name:"Food" , logo: "folder"))
+        .modelContainer(for: [Category.self, Phrase.self], inMemory: true)
+}
