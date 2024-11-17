e58a66b4dcc327344eb7c64e755164921302ed2c
Jonas Kaad <jonaskaad@gmail.com>
2024-11-17 13:56:40 +0900

    Renamed contextview

--- Phraser/View/ContextWidgetView.swift
+++ Phraser/View/ContextWidgetView.swift
@@ -1,5 +1,5 @@
 //
-//  ContextView.swift
+//  ContextWidgetView.swift
 //  Phraser
 //
 //  Created by Jonas Kaad on 16/11/2024.
@@ -7,33 +7,35 @@
 
 import SwiftUI
 
-struct ContextView: View {
+struct ContextWidgetView: View {
+    @StateObject private var locationManager = LocationManager()
+
     var body: some View {
         TimelineView(.periodic(from: .now, by: 1)) { context in
             VStack(alignment: .leading) {
                 HStack(spacing: 10) {
                     Image(systemName: "location")
-                        .font(.system(size: 30))
+                        .font(.system(size: 32))
                         .foregroundColor(.green)
-                    Text("DICE Dormitory")
-                        .font(.title3)
+                    Text(locationManager.address)
+                        .font(.title2)
                         .fontWeight(.bold)
                         .foregroundColor(.black)
                 }
                 HStack(spacing: 10) {
                     Image(systemName: "clock")
-                        .font(.system(size: 30))
-                        .foregroundColor(.purple)
+                        .font(.system(size: 32))
+                        .foregroundColor(.orange)
                     Text(currentTime())
-                        .font(.title3)
+                        .font(.title2)
                         .fontWeight(.bold)
                         .foregroundColor(.black)
                 }
             }
             .frame(maxWidth: .infinity, minHeight: 0, alignment: .leading)
             .padding(8)
-            .background(Color(UIColor.systemBackground))
             .cornerRadius(10)
+            .roundedCornersWithoutBG(color:.purple, lineWidth: 2)
             .padding(.horizontal, 10)
             .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
         }
@@ -41,10 +43,9 @@ struct ContextView: View {
 }
 
 func currentTime() -> String {
-    let currentHour = Calendar.current.component(.hour, from: Date())
-    let currentMinute = Calendar.current.component(.minute, from: Date())
-    let currentSecond = Calendar.current.component(.second, from: Date())
-    return "\(currentHour):\(currentMinute):\(currentSecond)"
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "HH:mm:ss"
+    return dateFormatter.string(from: Date())
 }
 
 #Preview{
