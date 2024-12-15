d82513ab41121345b1151d5f968bec88ab221350
Jonas Kaad <jonaskaad@gmail.com>
2024-12-15 13:16:51 +0900

    Implemented refresh mechanism in ui

--- Phraser/View/ContextualView.swift
+++ Phraser/View/ContextualView.swift
@@ -35,13 +35,12 @@ struct ContextualView: View {
 
 struct ContextualPhraseView: View {
     @Environment(\.modelContext) private var modelContext
-    //@State var phrases: [PhraseWrapper]
-   // let text: String
     @State private var phraseManager = PhraseFetchManager()
     @State private var isShowingAddSheet = false
-    @State private var searchText = ""
     @State private var notification: ToastNotification?
-    @State private var isLoading = true
+    @State private var isFirstLoad = true
+    @State private var isRefreshing = false
+    @State private var showCheckmark = false
     private let toastOptions = SimpleToastOptions(
         alignment: .bottom, hideAfter: 4
     )
@@ -58,17 +57,36 @@ struct ContextualPhraseView: View {
                             .scaleEffect(2)
                             .padding(.top, 10)
                             .tint(.purple)
-                        
                         Spacer()
-                        
-//                        Button("Load Phrases") {
-//                            isLoading = false
-//                            phraseManager.fetchPhrases()
-//                        }
                         Spacer()
                     }
                 } else {
                     VStack(alignment: .leading, spacing: 20) {
+                        if isRefreshing {
+                            Spacer()
+                            VStack {
+                                HStack {
+                                    Spacer()
+                                    ProgressView()
+                                        .progressViewStyle(CircularProgressViewStyle())
+                                        .tint(.blue)
+                                        .scaleEffect(2)
+                                    Spacer()
+                                }
+                            }
+                        } else if showCheckmark {
+                            Spacer()
+                            VStack {
+                                HStack {
+                                    Spacer()
+                                Image(systemName: "checkmark.circle")
+                                    .foregroundColor(.green)
+                                    .transition(.scale)  // Animation for showing/hiding
+                                    .scaleEffect(2)
+                                    Spacer()
+                                }
+                            }
+                        }
                         ScrollView {
                             ForEach(phraseManager.currentPhrases) { phrase in
                                 ContextualPhraseDisplayView(
@@ -78,33 +96,65 @@ struct ContextualPhraseView: View {
                                 )
                                 Spacer()
                             }
-                            Button("Refresh Phrases") {
-                                isLoading = false
-                                phraseManager.fetchPhrases(mode: "append")
-                            }
+                        }
+                        .refreshable {
+                            refreshPhrases()
                         }
                     }
                 }
             }
         }
         .onAppear(perform: {phraseManager.fetchPhrases()})
+        .onChange(of: phraseManager.refreshCompleted, {
+            // Only trigger if it's not the first load
+            if !isFirstLoad && phraseManager.refreshCompleted {
+                stopRefreshing()
+            }
+            
+            // Reset the first load flag after the initial load
+            if isFirstLoad {
+                isFirstLoad = false
+            }
+        })
         .onToastNotification {
             notification = $0
         }
+        
         .simpleToast(item: $notification, options: toastOptions) {
-            
-                HStack {
-                    Image(systemName: notification?.icon ?? "exclamationmark.triangle")
-                    Text(notification?.text ?? "Default message")
-                    
-                }
-                .padding()
-                .background(notification?.color.opacity(0.8))
-                .foregroundColor(Color.white)
+            HStack {
+                Image(systemName: notification?.icon ?? "exclamationmark.triangle")
+                Text(notification?.text ?? "Default message")
+                
+            }
+            .padding()
+            .background(notification?.color.opacity(0.8))
+            .foregroundColor(Color.white)
         }
     }
+    
+    // Refresh logic
+    private func refreshPhrases() {
+        isRefreshing = true
+        showCheckmark = false  // Reset checkmark visibility
+
+        phraseManager.fetchPhrases(mode: "refresh")
+    }
+
+    // Stops the refresh animation and shows the checkmark
+    private func stopRefreshing() {
+        isRefreshing = false
+        showCheckmark = true
+        
+        // Hide the checkmark after 1 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            withAnimation {
+                showCheckmark = false
+            }
+        }
+    }
+        
+    
 }
 #Preview{
-    let phrases: [PhraseWrapper] = [PhraseWrapper(phrase: "Good evening!", translation: "좋은 저녁", transliteration: "joeun jonyok")]
     ContextualPhraseView()
 }
